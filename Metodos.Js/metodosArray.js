a0a791a35cbb7ec68f912af81440be0582bb0e50
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-04-15 10:07:14 -0300

    feat: adicionando novas dicas e funcoes

---
+++ Metodos.Js/metodosArray.js
@@ -0,0 +1,35 @@
+//.lenght() => tamanho
+
+//let fruta = [
+// "Abacate", "Abacaxi", "Acerola", "Ameixa",
+//   "Banana",
+//  "Cajá Manga", "Carambola", "Caju", "Ciriguela", "Cupuaçu",
+// "Goiaba", "Graviola",
+// "Ingá-Cipó",
+//  "Maçã", "Mangostim", "Maracujá", "Morango",
+//  "Pitomba",
+// "Romã",
+// "Uva"
+// ];
+//console.log(frutas);
+
+//----------------------------------------------------------------------------------------
+
+//let numeros = [70, 100, 120, 1000];
+//numeros.reverse();  isso faz a ordem mudar a ordem (reverter ela)
+//console.log(numeros)
+
+//----------------------------------------------------------------------------------------
+
+let nomes = ["Valdemir", "Edson", "Anna", "Ana"];
+//.push() => adiciona um elemento no final do nosso array
+nomes.push("Matheus");
+
+//unshift() => adiciona um elemento no inicio do nosso array
+nomes.unshift("Sophia");
+
+//.pop() => remover no final
+nomes.pop();
+
+//para exibir os nomes finais
+console.log(nomes);
